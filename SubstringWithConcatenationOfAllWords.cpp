d3f918a8a338672d6069cfc0cdd1bca845c20b2d
alexandersdickinson <alexandersdickinson@gmail.com>
2017-08-05 12:49:44 -0500

    Handles repeated characters.

--- SubstringWithConcatenationOfAllWords.cpp
+++ SubstringWithConcatenationOfAllWords.cpp
@@ -4,19 +4,19 @@ public:
         //iterate through string
         std::vector<int> solution;
         std::vector<std::string> temp = words;
-        int startingIndex, firstMatchLength; 
+        int startingIndex; 
         bool match;
         for(int i = 0; i < s.length();){
             match = false;
             //test for a string in words
             for(auto it = temp.begin(); it != temp.end(); ++it){
-                //if current element in temp is equal to substring starting at i with same length as current element in temp, set match to true, break out of loop.
+                //if current element in temp is equal to substring starting at i with same length as current element in temp, set
+                //match to true, break out of loop.
                 //delete element from temp, reassign iterator, iterate i by the length of element in temp. Add i to solution vector.
                 if((*it) == s.substr(i, (*it).length())){
                     match = true;
                     if(temp.size() == words.size()){
                         startingIndex = i;
-                        firstMatchLength = (*it).length();
                     }
                     i += (*it).length();
                     it = temp.erase(it);
@@ -24,13 +24,13 @@ public:
                 }
             } 
             if(!match){
-                i = (temp.size() != words.size()) ? startingIndex + firstMatchLength : i + 1;
+                i = (temp.size() != words.size()) ? startingIndex + 1 : i + 1;
                 temp = words;
             }
             else if(temp.empty()){
                 solution.push_back(startingIndex);
                 temp = words;
-                i = startingIndex + firstMatchLength;
+                i = startingIndex + 1;
             }
         }
         return solution;
