e448a40482674b3488580b14989104fddb21a4a9
alexandersdickinson <alexandersdickinson@gmail.com>
2017-08-05 14:49:47 -0500

    Passes leetcode testcases.

--- SubstringWithConcatenationOfAllWords.cpp
+++ SubstringWithConcatenationOfAllWords.cpp
@@ -1,36 +1,28 @@
 class Solution {
 public:
     vector<int> findSubstring(string s, vector<string>& words) {
-        //iterate through string
         std::vector<int> solution;
-        std::vector<std::string> temp = words;
-        int startingIndex, wordLen = words.at(0).length(); 
-        bool match;
-        for(int i = 0; i < s.length() - wordLen + 1;){
-            match = false;
-            //test for a string in words
-            for(auto it = temp.begin(); it != temp.end(); ++it){
-                //if current element in temp is equal to substring starting at i with same length as current element in temp, set
-                //match to true, break out of loop.
-                //delete element from temp, reassign iterator, iterate i by the length of element in temp. Add i to solution vector.
-                if((*it) == s.substr(i, wordLen)){
-                    match = true;
-                    if(temp.size() == words.size()){
-                        startingIndex = i;
-                    }
-                    i += wordLen;
-                    it = temp.erase(it);
-                    break; 
+        //Number of words and the number of occurrences.
+        std::unordered_map<std::string, int> counts;
+        std::unordered_map<std::string, int> tempCounts;
+        int wordLen = words.at(0).length();
+        //Populate counts with words.
+        for(auto word = words.begin(); word != words.end(); ++word){
+            ++(counts[(*word)]); 
+        }
+        //Iterate through the string.
+        for(int i = 0; i < s.length() - wordLen * words.size() + 1; ++i){
+            tempCounts.clear();
+            for(int j = i; j - i < wordLen * words.size(); j += wordLen){
+                if(counts.find(s.substr(j, wordLen)) != counts.end()){
+                    ++(tempCounts[s.substr(j, wordLen)]);
                 }
-            } 
-            if(!match){
-                i = (temp.size() != words.size()) ? startingIndex + 1 : i + 1;
-                temp = words;
+                else{
+                    break;
+                }
             }
-            else if(temp.empty()){
-                solution.push_back(startingIndex);
-                temp = words;
-                i = startingIndex + 1;
+            if(counts == tempCounts){
+                solution.push_back(i); 
             }
         }
         return solution;
