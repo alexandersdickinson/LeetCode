71ce928dcaca705478a6e3f85a8400801c3f0049
alexandersdickinson <alexandersdickinson@gmail.com>
2017-08-06 15:09:25 -0500

    Incomplete custom implementation of NextPermutation.cpp.

--- NextPermutation.cpp
+++ NextPermutation.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        std::next_permutation(nums.begin(), nums.end());       
+        bool ordered = true;
+        for(auto it = nums.rbegin(); it != nums.rend() - 1; ++it){
+            //if the previous element is less than the current element
+            if((*it) > (*(it + 1))){
+                std::iter_swap(it, it + 1);
+                ordered = false;
+                break;
+            } 
+        }
+        if(ordered) std::sort(nums.begin(), nums.end());
     }
 };
