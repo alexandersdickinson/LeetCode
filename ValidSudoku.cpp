d381b1e603beeb31aa9ac38085e887874852152c
alexandersdickinson <alexandersdickinson@gmail.com>
2017-09-13 14:09:04 -0700

    Finish naive solution to ValidSudoku.cpp.

--- ValidSudoku.cpp
+++ ValidSudoku.cpp
@@ -1,56 +1,54 @@
 class Solution {
 public:
-    bool isValidSudoku(vector<vector<char>>& board) {
-        std::vector<char> nums = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+    bool isValidSudoku(std::vector<std::vector<char>>& board){
         //evaluate rows
+        //go down rows 
         for(auto row = board.begin(); row != board.end(); ++row){
-            std::vector<char> temp = nums;
-            for(auto element = row -> begin(); element != row -> end(); ++element){
-                for(auto num = temp.begin(); num != temp.end(); ++num){
-                    if(*element == *num){
-                        num = temp.erase(num);
-                    } 
+            std::unordered_map<char, int> numerals = std::unordered_map<char, int>();
+            //visit each square (go across columns) 
+            for(auto column = row -> begin(); column != row -> end(); ++column){
+                if(numerals[*column] > 0 && *column != '.'){
+                    return false; 
                 }
-            }
-            if(temp.size() != 0){
-                return false; 
-            }
-        } 
-        //evaluate columns
-        for(int col = int(); col < 9; ++col){
-            std::vector<char> temp = nums;
-            for(auto row = board.begin(); row != board.end(); ++row){
-                for(auto num = temp.begin(); num != temp.end(); ++num){
-                    if(row -> at(col) == *num){
-                        num = temp.erase(num); 
-                    } 
+                else{
+                    ++numerals[*column]; 
                 }
-            }
-            if(temp.size() != 0){
-                return false; 
             }
         }
-        //evaluate row of squares
-            //evaluate square (column)
-                //evaluate row inside square
-                    //evaluate element inside square
-                        //iterate through elements in temp, match to current element in square
-        for(int squareRow = int(); squareRow < 3; ++squareRow){
-            for(int square = int(); square < 3; ++square){
-                std::vector<char> temp = nums;
-                for(int row = int(); row < 3 + 3 * squareRow; ++row){
-                    for(int element = int(); element < 3 + 3 * square; ++element){
-                        for(auto num = temp.begin(); num != temp.end(); ++num){
-                            if(*num == board.at(row).at(element)){
-                                num = temp.erase(num); 
-                            } 
-                        }
-                    } 
-                } 
-                if(temp.size() != 0){
-                    return false; 
+
+        //evaluate columns
+        //go across columns
+        for(int column = int(); column < board.at(0).size(); ++column){
+            std::unordered_map<char, int> numerals = std::unordered_map<char, int>();
+            for(auto row = board.begin(); row != board.end(); ++row){
+                if(numerals[(*row).at(column)] > 0 && (*row).at(column) != '.'){
+                    return false;
+                }
+                else{
+                    ++numerals[(*row).at(column)]; 
                 }
             } 
         }
+
+        //evaluate 3 x 3 squares
+        //select square
+        //select square starting row
+        for(int squareRow = int(); squareRow < 3; ++squareRow){
+            //select square starting column  
+            for(int squareCol = int(); squareCol < 3; ++squareCol){
+                std::unordered_map<char, int> numerals = std::unordered_map<char, int>();
+                for(auto row = board.begin() + 3 * squareRow; row != board.begin() + 3 * squareRow + 3; ++row){
+                    for(auto column = row -> begin() + 3 * squareCol; column != row -> begin() + 3 * squareCol + 3; ++column){
+                        if(numerals[*column] > 0 && *column != '.'){
+                            return false; 
+                        }
+                        else{
+                            ++numerals[*column]; 
+                        }
+                    } 
+                }
+            }
+        }
+        return true;
     }
 };
